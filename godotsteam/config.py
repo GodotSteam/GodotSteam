728dfce813e32a49107ca2fb8685bab47b29554d
Xavier Sellier <xsellier@gmail.com>
2017-12-07 10:52:04 -0500

    Fix missing symbols when compiling under linux     Fix wrong argument to compile OSX under linux

--- godotsteam/config.py
+++ godotsteam/config.py
@@ -1,43 +1,34 @@
-
 def can_build(platform):
-	return platform=="x11" or platform=="windows" or platform=="osx"
+  return platform=="x11" or platform=="windows" or platform=="osx"
 
 def configure(env):
-	env.Append(CPPPATH=["#modules/godotsteam/sdk/public/"])
-	if env["platform"]== "x11":
-		env.Append(LIBS=["steam_api"])
-		#env.Append(RPATH=["."])
-		if env["bits"]=="32":
-			env.Append(RPATH=env.Literal('\\$$ORIGIN'))
-			env.Append(LIBPATH=["#modules/godotsteam/sdk/redistributable_bin/linux32"])
-		elif env["bits"]=="64":
-			env.Append(RPATH=env.Literal('\\$$ORIGIN'))
-			env.Append(LIBPATH=["#modules/godotsteam/sdk/redistributable_bin/linux64"])
-	elif env["platform"] == "osx":
-		env.Append(LIBS=["steam_api"])
-		env.Append(RPATH=env.Literal('\\$$ORIGIN'))
-		env.Append(LIBPATH=["#modules/godotsteam/sdk/redistributable_bin/osx32"])
-	elif env["platform"] == "windows":
-		steamlib = "steam_api"
-		steamlib_path = "#modules/godotsteam/sdk/redistributable_bin"
-		# use 64bit steam_api on Windows 64bit?
-		use_64bit_lib = True # Change to False if you have problems with "unresolved external symbols"
-		
-		if env["CC"] == "cl": # mostly VisualStudio
-			if env["bits"] == "32":
-				steamlib += ".lib" # steam_api.lib
-			elif env["bits"] == "64":
-				if (use_64bit_lib):
-					steamlib += "64" # steam_api64 (+.lib)
-					steamlib_path += "/win64" # .../sdk/redistributable_bin/win64
-				steamlib += ".lib"
-			env.Append(LINKFLAGS=[ steamlib ])
-		else: # mostly "gcc"
-			# 32bit - stays "steam_api"
-			if env["bits"] == "64":
-				if (use_64bit_lib):
-					steamlib += "64" # steam_api64
-					steamlib_path += "/win64" # .../sdk/redistributable_bin/win64
-			env.Append(LIBS=[ steamlib ])
-		
-		env.Append(LIBPATH=[ steamlib_path ])
+  env.Append(CPPPATH=["#modules/godotsteam/sdk/public/"])
+  if env["platform"]== "x11":
+    env.Append(LIBS=["steam_api"])
+    #env.Append(RPATH=["."])
+    if env["bits"]=="32":
+      env.Append(RPATH=env.Literal('\\$$ORIGIN/linux32'))
+      env.Append(LIBPATH=["#modules/godotsteam/sdk/redistributable_bin/linux32"])
+    else: # 64 bit
+      env.Append(RPATH=env.Literal('\\$$ORIGIN/linux64'))
+      env.Append(LIBPATH=["#modules/godotsteam/sdk/redistributable_bin/linux64"])
+  elif env["platform"] == "windows":
+    # mostly VisualStudio
+    if env["CC"] == "cl":
+      if env["bits"]=="32":
+        env.Append(LINKFLAGS=["steam_api.lib"])
+        env.Append(LIBPATH=["#modules/godotsteam/sdk/redistributable_bin"])
+      else: # 64 bit
+        env.Append(LINKFLAGS=["steam_api64.lib"])
+        env.Append(LIBPATH=["#modules/godotsteam/sdk/redistributable_bin/win64"])
+    # mostly "gcc"
+    else:
+      if env["bits"]=="32":
+        env.Append(LIBS=["steam_api"])
+        env.Append(LIBPATH=["#modules/godotsteam/sdk/redistributable_bin"])
+      else: # 64 bit
+        env.Append(LIBS=["steam_api64"])
+        env.Append(LIBPATH=["#modules/godotsteam/sdk/redistributable_bin/win64"])
+  elif env["platform"] == "osx":
+    env.Append(LIBS=["steam_api"])
+    env.Append(LIBPATH=['#modules/godotsteam/sdk/redistributable_bin/osx32'])
