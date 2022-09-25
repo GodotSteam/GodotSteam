e70a1b5d4062c05aa3d004bf88d5851f1db34db7
GP Garcia <tenthandhess@gmail.com>
2022-09-25 11:34:15 -0500

    Fixed compiling for Linux

--- godotsteam/config.py
+++ godotsteam/config.py
@@ -1,5 +1,5 @@
 def can_build(env, platform):
-	return platform=="x11" or platform=="windows" or platform=="osx" or platform=="server"
+	return platform=="linuxbsd" or platform=="windows" or platform=="osx" or platform=="server"
 
 def configure(env):
 	pass
