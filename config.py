b271cda2d8a4afe219440d0aed34b742244813fb
Dragos Daian <daian.dragos@yahoo.com>
2025-01-21 21:29:41 +0100

    remove headers and make it not build if headers are missing.

--- config.py
+++ config.py
@@ -1,5 +1,10 @@
+import os.path
+
 def can_build(env, platform):
-	return platform=="linuxbsd" or platform=="windows" or platform=="macos" or platform=="server"
+	# Check if headers are present
+	if os.path.exists("sdk/public/steam/steam_api.h"):
+		return platform=="linuxbsd" or platform=="windows" or platform=="macos" or platform=="server"
+	return False
 
 def configure(env):
 	pass
