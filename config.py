0bfb4c183bce6fde1b1cfa052d1a096ab5fce362
Dragos Daian <daian.dragos@yahoo.com>
2025-01-28 11:40:45 +0100

    build on all platforms since it won't crash if missing dll.

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ module_path = os.path.dirname(os.path.realpath(__file__))
 def can_build(env, platform):
 	# Check if headers are present
 	if os.path.exists("%s/sdk/public/steam/steam_api.h" % module_path):
-		return platform=="linuxbsd" or platform=="windows" or platform=="macos" or platform=="server"
+		return True
 	return False
 
 def configure(env):
